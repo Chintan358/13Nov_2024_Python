cf0f020d2a1093e19ba4f5e6262d2001cf1515b6
chintan <chintan.tops@gmail.com>
2025-02-13 14:56:53 +0530

    all

--- 008_Django/Reportcard/studentapp/urls.py
+++ 008_Django/Reportcard/studentapp/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 from studentapp.views import *
 urlpatterns = [
-    path("",index,name="index")
+    path("",index,name="index"),
+    path("marks/<id>",marks,name="marks")
 ]
--- 008_Django/Reportcard/studentapp/views.py
+++ 008_Django/Reportcard/studentapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from studentapp.models import *
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    studentData = Student.objects.all()
+
+    paginator = Paginator(studentData, 5) 
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+   
+    return render(request, 'index.html',{"studentData":page_obj})
+
+def marks(request,id):
+    studentId = StudentId.objects.get(student_id=id)
+    student = Student.objects.get(student_id=studentId)
+    studentMarks = Marks.objects.filter(student=student)
+    return render(request, 'marks.html', {"studentMarks":studentMarks})
