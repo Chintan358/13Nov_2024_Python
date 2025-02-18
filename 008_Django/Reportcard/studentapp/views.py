6f8d78329d3c3c40bd6733728d4b774c6d602dcf
chintan <chintan@gmail.com>
2025-02-18 08:49:17 +0530

    all

--- 008_Django/Reportcard/studentapp/views.py
+++ 008_Django/Reportcard/studentapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from studentapp.models import *
 from django.core.paginator import Paginator
+from django.db.models import Sum
 # Create your views here.
 def index(request):
     studentData = Student.objects.all()
@@ -12,7 +13,21 @@ def index(request):
     return render(request, 'index.html',{"studentData":page_obj})
 
 def marks(request,id):
-    studentId = StudentId.objects.get(student_id=id)
-    student = Student.objects.get(student_id=studentId)
-    studentMarks = Marks.objects.filter(student=student)
-    return render(request, 'marks.html', {"studentMarks":studentMarks})
+    # studentId = StudentId.objects.get(student_id=id)
+    # student = Student.objects.get(student_id=studentId)
+
+    rank =  Student.objects.annotate(totalmarks = Sum("marks__marks")).order_by('-totalmarks')
+    count = 0
+    for i in rank:
+        # print(i.totalmarks,i.student_id.student_id)
+        count+=1
+        if i.student_id.student_id==id:
+            print(i.totalmarks)
+            break
+  
+    studentMarks = Marks.objects.filter(student__student_id__student_id=id)
+    std = studentMarks[0].student
+    total =  studentMarks.aggregate(total = Sum("marks"))
+   
+
+    return render(request, 'marks.html', {"studentMarks":studentMarks,"std":std,"total":total,"count":count})
